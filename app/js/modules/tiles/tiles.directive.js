8dd47924366347ddb3fb26f1ec9213b1dd8cac55
Russell Wenban <russell@factornine.co.uk>
2015-10-12 21:13:32 +0100

    Add Drag UI - Trello #35 – Add draggable – Add drop zone ( Can drop into drop zone, but not re-order or put back)

--- app/js/modules/tiles/tiles.directive.js
+++ app/js/modules/tiles/tiles.directive.js
@@ -11,7 +11,7 @@
             scope: {
             },
             controller: TilesController,
-            controllerAs: 'vm',
+            controllerAs: 'dragDrop',
             bindToController: true, // because the scope is isolated
             template: $templateCache.get('modules/tiles/tiles.html'),
             link: tilesLink
@@ -21,17 +21,92 @@
 
 
             // '...is not needed when the function is named using UpperCasing, as this convention means it is a constructor function, which is what a controller is in Angular...'
-            var vm = this;
+            var dragDrop = this;
 
             $log.info('Tiles setting up Store event!');
-            vm.tiles = MyStore.tiles;
+            dragDrop.tiles = MyStore.tiles;
 
 
             $scope.$listenTo(MyStore, 'tile.*', function () {
                 $log.info('tile.add');
-                vm.tiles = MyStore.tiles;
+                dragDrop.tiles = MyStore.tiles;
+
+                // set the list of drag and drop items here...
+                dragDrop.dragItems = angular.copy(dragDrop.tiles);
+
+                dragDrop.dropItems = [];
             });
+
+
+
+
+
+            dragDrop.currentDragItem = undefined;
+
+
+            dragDrop.list5 = [
+                { 'title': 'Item 1', 'drag': true },
+                { 'title': 'Item 2', 'drag': true },
+                { 'title': 'Item 3', 'drag': true },
+                { 'title': 'Item 4', 'drag': true },
+                { 'title': 'Item 5', 'drag': true },
+                { 'title': 'Item 6', 'drag': true },
+                { 'title': 'Item 7', 'drag': true },
+                { 'title': 'Item 8', 'drag': true }
+            ];
+
+            dragDrop.list2 = [];
+
+
+
+            // drag functions
+            dragDrop.startCallback = function(event, obj, tile, index) {
+
+                dragDrop.currentDragItem = tile;
+            };
+
+            dragDrop.dragCallback = function(event, obj, tile, index) {
+               // $log.info('** dragDrop.onDrag | tile: ', tile)
+            };
+
+            dragDrop.stopCallback = function(event, obj, tile, index) {
+               // $log.info('** dragDrop.onStop | tile: ', tile);
+            };
+
+
+            // drop functions
+            dragDrop.dropCallback = function(event, obj, tile, index) {
+                //$log.info('A dragDrop.dropCallback| tile: ', arguments);
+                //$log.info('B dragDrop.dropCallback| tile: ', tile);
+                //$log.info('B dragDrop.dropCallback| currentDragItem: ', dragDrop.currentDragItem);
+
+                _showItems();
+
+                _addToDropItems(dragDrop.currentDragItem)
+            };
+
+            function _addToDropItems(item) {
+                $log.info('add item: ', arguments)
+
+                console.log('-(  adding... ', dragDrop.dropItems[0])
+
+                dragDrop.dropItems.push(item);
+
+                console.log('adding... ', dragDrop.dropItems.length)
+            }
+
+
+            function _showItems() {
+
+                $log.info('------------- SHOW ITEMS ------------\n')
+
+                $log.info('drag: ',dragDrop.dragItems);
+                $log.info('drop: ', dragDrop.dropItems);
+
+            }
         }
+
+
 
         function tilesLink() {
         }
